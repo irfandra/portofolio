d29bccecbd8b50e2535759575730f892d3ff73bc
Irfan Rahmanindra <58932978+irfandra@users.noreply.github.com>
2026-09-14 18:28:08 +0700

    add analytic

--- irfan-portofolio/src/app/layout.js
+++ irfan-portofolio/src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
