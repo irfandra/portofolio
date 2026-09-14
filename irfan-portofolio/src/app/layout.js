2553e71b3eba8adca57e2910980599fc11dcf697
Irfan Rahmanindra <58932978+irfandra@users.noreply.github.com>
2026-09-14 18:58:17 +0700

    add analystic

--- irfan-portofolio/src/app/layout.js
+++ irfan-portofolio/src/app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
       <body className="bg-black">
         <ThemeProvider>
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
